631a906b3c69b3338f17d3f0b544ba21eef700e4
Steven Li <stevenjli@gmail.com>
2018-05-15 22:02:38 -0700

    Expiring jwt tokens

--- app/controllers/api/v1/sensitive_data_controller.rb
+++ app/controllers/api/v1/sensitive_data_controller.rb
@@ -13,10 +13,20 @@ class Api::V1::SensitiveDataController < ApplicationController
   def authenticate!
     auth_token = request.headers["Auth-Token"]
     decrypted_data = JwtDecodingService.new(auth_token).decrypt!
+    if decrypted_data["iat"] < Time.now.to_i - 3 * months_in_seconds
+      render json: {
+        error: "invalid auth token"
+      }, status: 401
+      return
+    end
     @current_user = User.find(decrypted_data["sub"])
   rescue JWT::DecodeError
     render json: {
       error: "invalid auth token"
     }, status: 401
+  end
+
+  def months_in_seconds
+    60 * 24 * 30
   end
 end
